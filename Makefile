f538d872382595cbff1a0786e27767eeb4e2bf6a
shaibruhis <shaibruhis@gmail.com>
2015-04-02 17:30:03 -0700

    Pseudocode for how we will attack the project. woohoo

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ ALL:
 	g++ ashell.cpp -o ashell.out && ./ashell.out
 	
 clean:
-	rm -rf ashell.out
+	rm -rf ashell.out ashell.o
 
 r:
 	./ashell.out
--- ashell.cpp
+++ ashell.cpp
@@ -5,6 +5,9 @@
 #include <queue>
 using namespace std;
 
+// store file path in stack of strings leading with a '/'
+
+
 //char array for current working directory absolute path
 char currentDirectory[100];
 //queue of char arrays that will correspond to history of commands
@@ -38,3 +41,42 @@ int main (int argc, char** argv) {
 
 
 // get current working directory: char *getcwd(char *buf, size_t size);
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+// -----DATA TYPES-----
+// int historyLocation      - current index in history (valid between -1 and 9)
+
+// char[] currentDirectory  - the string of the current path
+// char[] startingDirectory - the string of the initial path used to go back to when 'exit'
+// deque<string> history    - history of commands
+// stack<string> filepath   - currentDirectory delimited by '/' for easy filepath traversal
+
+
+
+
+// -----FUNCTIONS------
+// execute: 'execl()'
+
+// pipes: 'pipe()'
+
+// IO redirection:
+
+// cd: check if path is valid, if it is then push each '/name' to stack and pop off stack if '/..'
+
+// ls: check if path is valid, if it is then print contents of directory. if no path specified then
+// print contents of currentDirectory. referenced: http://stackoverflow.com/questions/10323060/printing-file-permissions-like-ls-l-using-stat2-in-c
+
+// pwd: print out currentDirectory path
+
+// history: each command (valid or invalid) will be pushed to front of a deque. 
+// commands past 9 will be popped off the back. global var stored to keep track
+// of where you are in the history. If at -1, write blank line. If at -2 stay
+// at blank line and output alert sound ('\a')
+
+// exit: return 0 in main()
+
