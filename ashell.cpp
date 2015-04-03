621b39c199567e46d4c4ccc3fdf3f805343fdb29
Stefan Peterson <stefan.t.peterson@gmail.com>
2015-04-03 16:40:23 -0700

    changes I made in class today

--- ashell.cpp
+++ ashell.cpp
@@ -1,70 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <deque>
 using namespace std;
 
+bool exitStatus = 1;
+//start directory
+char command[100];
 //start directory
 char startDirectory[100];
 //char array for current working directory absolute path
 char currentDirectory[100];
 //queue of char arrays that will correspond to history of commands
-deque<char*> history;
+deque<string> listedHistory;
 //vector of char arrays
-stack<string> delimitedCurrDirectory;
+// stack<string> delimitedCurrDirectory;
+
 
 //gets current directory absolute path name
 void getCurrentDirectory(){
 	getcwd(currentDirectory, 100);
 }
 
-void delimit(char *filepath, int size){
-	for(int i = 0; i < size; i++) {
-		if(filepath[i] == '/')
-			push
-	}
+// void delimit(char *filepath, int size){
+// 	for(int i = 0; i < size; i++) {
+// 		if(filepath[i] == '/')
+// 			push
+// 	}
+
+// }
+
+void cd();
+void ls();
+
+
+void pwd() {
+	write(1, currentDirectory, 100);
+	write(1, "\n", 2);
+}
+
+
+void history() {  
+  std::deque<string>::iterator it = listedHistory.begin();
+  	while (it != listedHistory.end()) {
+ 	cout << *it << endl;
+  		// write(1, *it, 100);
+  	}
 
 }
+
 
 void getCommand(){
-
-	ssize_t read(int fildes, void *buf, size_t nbyte);
-
+	write(1, "/.../ECS150>",12);
+	read(0, command, 100);
+	string temp(command);
+	//exits the program
+	if(temp == "exit\n") exitStatus = 0;
+	else if(temp == "pwd\n") pwd();
+	else if(temp == "ls\n") ls();
+	else if(temp == "cd\n") cd();
+	else if(temp == "history\n") history();
+	listedHistory.push_front(temp);
 }
 
-void printPermissions(){
-    struct stat fileStat;
+// void printPermissions(){
+//     struct stat fileStat;
 
 	
-	printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
-    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
-}
+// 	printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+//     printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+//     printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+//     printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+//     printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+//     printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+//     printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+//     printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+//     printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+//     printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+// }
 
 
 // Location currLoc();
-// void cd();
-// void ls();
-// void pwd();
-// void history();
+
 // void exit();
 
 
 int main (int argc, char** argv) {
 	getCurrentDirectory();
-	for(int i = 0; i < 100; i++ )
-		cout << currentDirectory[i];
+	// for(int i = 0; i < 100; i++ )
+	// 	cout << currentDirectory[i];
+	// cout << endl;
 
-	cout << endl;
+	while(exitStatus){
+		getCommand();
+	}
     return 0;
 }
 
@@ -113,4 +145,15 @@ int main (int argc, char** argv) {
 // at blank line and output alert sound ('\a')
 
 // exit: return 0 in main()
+
+
+
+
+
+
+// NITTAS BULLSHIT:
+// read(STDIN_FILENO, Buffer, size); //STDIN_FILENO = 0 generally
+// write(1, Buffer, size);
+// "\b \b"
+// 0x08 and 0x7F treat them the same
 
