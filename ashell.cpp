c7ed488696c2fae980091bcac45bad78fe7b16b3
shaibruhis <shaibruhis@gmail.com>
2015-04-11 17:40:09 -0700

    started working on redirection

--- ashell.cpp
+++ ashell.cpp
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <fcntl.h>
 #include "noncanmode.c"
 #include <deque>
 #include <stack>
@@ -136,7 +137,9 @@ void writePrompt() {
 	write(1, prompt, size);
 }
 
-
+// if you do:
+// cd
+// It will change the directory to the HOME directory. You definitely can use chdir().
 void cd() {
 	// int ret = chdir(com)
 }
@@ -240,11 +243,65 @@ void execute(string temp) {
 	else if(temp == "history\n") history();
 	else {
 		// have to convert vector to array to be able to pass into execvp. instead of copying 
-		// vector i just created a pointed to a char * and set it to front of vector.
+		// vector I just created a pointer to a char * and set it to front of vector.
 		char** argArray = &argVector[0];
 		execvp(argArray[0], argArray);
 	}
 }
+
+void redirectIn() {
+	char* path_name;
+	for (int i = 0; i < delimitedCommand.size(); ++i)
+	{
+		if (delimitedCommand[i] == "<")
+		{
+			path_name = delimitedCommand[i+1];
+		}	
+	}
+	int fd = open(path_name, O_WRONLY | O_CREAT);
+	dup2(fd, 0);
+	close(fd);
+}
+
+void redirectOut() {
+	char* path_name;
+	for (int i = 0; i < delimitedCommand.size(); ++i)
+	{
+		if (delimitedCommand[i] == ">")
+		{
+			path_name = delimitedCommand[i-1];
+		}	
+	}
+	int fd = open(path_name, O_RDONLY);
+	dup2(fd, 1);
+	close(fd);
+}
+
+// To answer your question, no, pipe and dup2 are for redirecting the standard input/output from a child to a parent, or child to child, etc. All input/output is read/written using read() and write() ultimately. Even if you are using printf, or scanf it ultimately calls read or write. The reason for dup2() is to duplicate a file descriptor. Remember the file descriptors are just indices into the array of open files (the OS maintains this array). What dup2 does is copies one of the entries in the array to another location. So if you want to redirect the output of a program to output to a file you can: 
+// 1. Open the file with open() to get a file descriptor for the file
+// 2. Call dup2() to redirect the file descriptor to STDOUT_FILENO (also happens to be file descriptor 1).
+// 3. Close the original file descriptor returned by open(). This entry in the array isn't needed anymore.
+// 4. Continue executing the program with all standard out going to the new file. Alternatively you can make the program "become" another program through a call to exec(), execvp(), or execlp(), etc.
+ 
+// The pipe() call creates a pipe. A pipe is just a FIFO what ever is written in one end gets read out the other end. We use piping to redirect the output of one program into the input of another. Lets say you want to dump the contents of main.cpp, and grep the lines that have void in them. From a shell you could use the command:
+// cat main.cpp | grep void
+// This is what will happen, and outline of how your shell should handle this:
+// 1. Parent shell displays command prompt.
+// 2. User enters: cat main.cpp | grep void
+// 3. Parent shell interprets the input and determines to create children and a pipe.
+// 4. Parent creates a pipe so that the children (cat and grep) can use them.
+// 5. Parent shell will fork() to create child (cat).
+//   1C1. Child will redirect standard out STDOUT_FILENO (or file descriptor 1) to point to the write end of the pipe [1] using dup2().
+//   2C1. Child optionally can close pipe ends, it doesn't need those file descriptors anymore.
+//   3C1. Child calls execvp() (or some derivative) to become cat with arguments cat, and main.cpp
+//   4C1. Child is now cat and output will be directed into the pipe
+// 6. Parent shell will fork() again to create child (grep).
+//   1C2. Child will redirect standard in STDIN_FILENO (or file descriptor 0) to point to the write end of the pipe [0] using dup2().
+//   2C2. Child optionally can close pipe ends, it doesn't need those file descriptors anymore.
+//   3C2. Child calls execvp() (or some derivative) to become grep with arguments grep, and void
+//   4C2. Child is now grep and input comes from the pipe
+// 7. Parent waits for both children to terminate using wait() or waitpid()
+// 8. Parent displays command prompt again.
 
 // handles the forking/piping/duping of commands
 void checkCommandType() {
@@ -254,7 +311,19 @@ void checkCommandType() {
 		execute(temp);
 	}
 	// no redirection requires no dup2() or pipe()
-	else if (temp.find("|") == string::npos && temp.find("<") == string::npos && temp.find(">") == string::npos) {
+	else if (temp.find("|") == string::npos) {
+	
+
+	// reading file in
+	if(temp.find("<") != string::npos) {
+		redirectIn();
+	}
+	// outputting to file
+	else if(temp.find(">") != string::npos) {
+		redirectOut();
+	}
+
+	
 		// http://timmurphy.org/2014/04/26/using-fork-in-cc-a-minimum-working-example/
 		pid_t pid = fork();
 		// child process
@@ -265,10 +334,8 @@ void checkCommandType() {
 		}
 		// parent process
 		else if (pid > 0) {
-			// wait for children to complete then return. PROBLEM: exit doesn't work because it only
-			// changes exitStatus in child process. global vars aren't shared between processes so 
-			// we need to return the exitStatus from the child processes i.e. "exit(0)". Also, if the
-			// command contains a "&" at the end we dont need to wait b/c it's running in bg.
+			// wait for children to complete then return. if the command contains a "&" 
+			// at the end we dont need to wait b/c it's running in bg.
 			// if (strcmp(argVector.back(), "&")) 
 			// {
 				wait(NULL);
